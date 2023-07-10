e39f8701e2f2a8177bc104f84af785381b25aae1
Youngkwan-Cho <dudrhks1009@naver.com>
2023-07-10 14:50:31 +0900

    Create C028_조영관_20230710.cpp

---
+++ 조영관_0710/C028_조영관_20230710.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  cin.tie(NULL); //입출력 묶음 해제
+  ios_base::sync_with_stdio(false);
+  
+  int t,num1,num2;
+  cin>>t;
+
+  for(int i=0;i<t;i++){
+      cin>>num1>>num2;
+      cout<<num1+num2<<"\n";
+  }
+
+  return 0;
+}
